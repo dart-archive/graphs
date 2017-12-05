b92e49bb48a026792b86a8f3c132272b1114b797
Nate Bosch <nbosch@google.com>
2017-12-05 10:53:55 -0800

    review comments

--- lib/src/crawl_async.dart
+++ lib/src/crawl_async.dart
@@ -4,7 +4,7 @@
 
 import 'dart:async';
 
-final _empty = new Future.value(null);
+final _empty = new Future<Null>.value(null);
 
 /// Finds and returns every node in a graph who's nodes and edges are
 /// asynchronously resolved.
@@ -22,8 +22,7 @@ final _empty = new Future.value(null);
 /// guarantees that the work is done in topological order.
 Stream<V> crawlAsync<K, V>(Iterable<K> roots, FutureOr<V> Function(K) readNode,
     FutureOr<Iterable<K>> Function(K, V) children) {
-  final crawl = new _CrawlAsync(roots, readNode, children);
-  crawl.run();
+  final crawl = new _CrawlAsync(roots, readNode, children)..run();
   return crawl.result.stream;
 }
 
@@ -40,14 +39,14 @@ class _CrawlAsync<K, V> {
 
   /// Add all nodes in the graph to [result] and return a Future which fires
   /// after all nodes have been seen.
-  Future run() async {
+  Future<Null> run() async {
     await Future.wait(roots.map(_visit));
     await result.close();
   }
 
   /// Resolve the node at [key] and output it, then start crawling all of it's
   /// children.
-  Future _crawlFrom(K key) async {
+  Future<Null> _crawlFrom(K key) async {
     var value = await readNode(key);
     result.add(value);
     var next = await children(key, value) ?? const [];
@@ -60,7 +59,7 @@ class _CrawlAsync<K, V> {
   /// The returned Future will complete only after the work for [key] and all
   /// transitively reachable nodes has either been finished, or will be finished
   /// by some other Future in [_seen].
-  Future _visit(K key) {
+  Future<Null> _visit(K key) {
     if (_seen.contains(key)) return _empty;
     _seen.add(key);
     return _crawlFrom(key);
